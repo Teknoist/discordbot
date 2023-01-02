ca4a3320f98543ad140702db17e6089847498975
Teknoist <37031361+Teknoist@users.noreply.github.com>
2023-01-02 23:39:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,6 @@ sudo apt-get install
 #Gerekli yazılımları yükle
 sudo apt-get -y install git python3 python3-pip etherwake
 
-#Github repoyu klonla
-sudo git clone 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@github.com:Teknoist/discordbot.git
-
 #Gereklilikleri yükle
-python3 -m pip install -r ./discordbot/requirements.txt
-
-sudo rm ./discordbot/requirements.txt
-sudo rm ./discordbot/install.sh
+python3 -m pip install -r ./requirements.txt
+sudo python3 -m pip install -r ./requirements.txt
