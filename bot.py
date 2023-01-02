f6ed955dde7d45d716c174cc021096c0eb43cd3d
Teknoist <37031361+Teknoist@users.noreply.github.com>
2023-01-02 23:33:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,64 +1,41 @@
-import subprocess
 import discord
 from discord.ext import commands
-from yeelight import Bulb
 import os
-bulb = Bulb("192.168.1.6")
 
+from yeelight import Bulb
+bulb = Bulb("192.168.1.6")
 
 intents = discord.Intents().all()
 intents.messages = True
 
-bot = commands.Bot(command_prefix="/", intents=intents)
-#bot = commands.Bot(command_prefix="/")
-
-
-
-#Deneme
-@bot.command()
-async def merhaba(ctx):
-	await ctx.send("Merhaba Dünyalı")
-
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 #Işık kontrolleri
-@bot.command()
-async def nox(ctx):
-	await ctx.send("Nox")
-	bulb.turn_off()
-	
 @bot.command()
 async def lumos(ctx):
 	await ctx.send("Lumos")
 	bulb.turn_on()
 
-
-#Bilgisayar kontrolleri
 @bot.command()
-async def pcoff(ctx):
-	await ctx.send("Bilgisayar Kapatılıyor")
-	print(subprocess.run(["/home/pi/pwof.sh",
-                "arguments"], shell=False))
+async def nox(ctx):
+	await ctx.send("nox")
+	bulb.turn_off()
+
+#Pc kontrolleri
 @bot.command()
-async def pcon(ctx):
-	await ctx.send("Bilgisayar Açılıyor")
-	print(subprocess.run(["/home/pi/wol.sh",
-                "arguments"], shell=False))
+async def on(ctx):
+	await ctx.send("Bilgisayar açıldı")
+	os.system('sudo etherwake -i eth0 80:FA:5B:20:7C:1C')
 
-#Youtube İndirme Yöneticisi
-
+#İndirme Yöneticisi
 @bot.command()
 async def ytl(ctx, arg1):
 	await ctx.send("İndirme Başlatıldı")
 	command = 'python3 yt-down.py ' + arg1
 	os.system(command)
 
-	
-
-
 #Anahtar
 @bot.event
 async def on_ready():
-    await bot.get_channel(1006637872331370506).send("Bot geldi HANIMMM")
+    await bot.get_channel(1006637872331370506).send("İndirme Modülü Aktif")
 bot.run("MTA0MDg3Njk5MDU0NDIzNjYzNQ.G-_NTZ.dTKGzadQDs-XSyHXXmlHDZVaRI-NWIUyob6pOY")
-
-
